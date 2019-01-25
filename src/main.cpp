ccded7e64a1767256bd6648e173074e22aaa0cc8
Nat <nat@cmmc.io>
2019-01-25 22:51:24 +0700

    Serial ok.

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,29 @@
 
 U8G2_ST7920_128X64_F_SW_SPI *u8g2;
 void setup() {
-    Serial2.begin(9600);
+    Serial.begin(115200);
+    Serial2.begin(9600, SERIAL_8N1,  16 /* rx */,  17 /* tx */);
     Serial2.setTimeout(10);
     u8g2 = new U8G2_ST7920_128X64_F_SW_SPI(U8G2_R0, /* clock=*/ 18, /* MOSI=*/ 23, /* MISO=*/ 19);
     u8g2->begin();
+    // displayLogo(); }
+    Serial.println("HELLO WORLD.");
+    Serial2.flush();
+    u8g2->setFont(u8g2_font_micro_tr);
+    u8g2->print("HELLO..");
 }
 
 void loop() {
+      while(Serial2.available()) {
+        Serial2.readStringUntil('+');
+        delay(10);
+        String o = Serial2.readStringUntil('\n');
+        Serial.print(o);
+        u8g2->clear();
         u8g2->firstPage();
         do {
           u8g2->setFont(u8g2_font_micro_tr);
-          // // u8g2->setCursor(logoMargin+10, 16);
-          u8g2->setCursor(0, 10);
-          u8g2->print("HELLO WORLD ");
-          u8g2->print(millis());
+          u8g2->print(o.c_str());
         } while (u8g2->nextPage());
       }
+}
